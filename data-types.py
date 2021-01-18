35aa52203e5e1b914a089377edef44b17b5f31d9
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-01-17 22:45:20 -0600

    data-types into

---
+++ data-types.py
@@ -0,0 +1,16 @@
+#These are the fundamental data-types to know in Python
+
+#Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> Custom Types
+
+#Specialized Data Types
+# => (modules and packages we can use from libraries)
