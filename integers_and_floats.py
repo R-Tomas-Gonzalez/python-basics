c09e5ecf819004fbb46d30bb07ccc193a0edeef8
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-01-17 22:46:39 -0600

    integers and floats intro

---
+++ integers_and_floats.py
@@ -0,0 +1,23 @@
+# print(type(6)) #int
+# print(type(2 - 4)) #int
+# print(type(2 * 4)) #int
+# print(type(2 / 4)) #int
+
+# print(2 ** 3) #2^3 operator
+# print(5 // 4) #rounds to the lowest int
+# print(6 % 4) #modulo operator (grabs the remainder)
+
+# # example math functions
+
+# print(round(3.6)) #rounds the number
+# print(abs(-4.5)) #returns the absolute value of a number
+
+# name = input("Hi, what is your name? ")
+
+# age = input(f"Hi {name}, how old are you? ")
+
+# print(f'Nice {name}, you are {age} years old!')
+# print(f"Have a great day, {name}")
+
+string = 'hello'
+print(string[-1])
