41ccf9cf2675f9108ea293d704c279c38cc0c759
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2021-01-17 22:57:15 -0600

    tuple intro

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#Tuple
+#immutible lists
+
+my_tuple = (1,2,3,4,5)
+
+# my_tuple[1] = 'z' #Doesn't work
+
+#You may access a tuple like below and normal ways you would in a list
+
+print(5 in my_tuple)
+
+#tuples are faster than lists
+#a good use is latitude or longitude
+#tuples come up in dictionary items
